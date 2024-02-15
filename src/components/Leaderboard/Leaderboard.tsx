f3c88ef0556f005d91cd26f0a1e5ad77287c0704
gio <95066222+gioAlgorithm@users.noreply.github.com>
2024-02-15 17:12:30 +0400

    fix fonts of start

--- src/components/Leaderboard/Leaderboard.tsx
+++ src/components/Leaderboard/Leaderboard.tsx
@@ -2,12 +2,18 @@
 import React from 'react'
 import style from "./Leaderboard.module.scss"
 import Link from 'next/link'
+import { Quantico } from 'next/font/google'
+
+const quantico = Quantico({
+  weight: ["400", '700'],
+  subsets: ['latin']
+})
 
 const Leaderboard = () => {
 
   return (
     <Link href="/leaderboardPage" className={style.leaderboard}>
-      <h1>Leaderboard</h1>
+      <h1 className={quantico.className}>Leaderboard</h1>
     </Link>
   )
 }
--- src/components/Start/Start.tsx
+++ src/components/Start/Start.tsx
@@ -1,6 +1,19 @@
 "use client"
 import React from 'react';
 import style from "./Start.module.scss";
+import { Black_Ops_One } from 'next/font/google';
+import { Russo_One } from 'next/font/google';
+
+const blackOps = Black_Ops_One({
+  weight: ["400"],
+  subsets: ['latin']
+})
+
+const russoOne = Russo_One({
+  weight: ['400'],
+  subsets:['latin']
+})
+
 
 interface Props {
   setStartGame: React.Dispatch<React.SetStateAction<boolean>>;
@@ -21,8 +34,8 @@ const Start: React.FC<Props> = ({ setStartGame, setLevel, startTimer, setNewReco
   return (
     <div className={style.startContainer}>
       <div className={style.innerStart}>
-        <h1>THE MAZE</h1>
-        <p>Guide the red point through the maze, but don&apos;t touch the walls!</p>
+        <h1 className={blackOps.className}>THE MAZE</h1>
+        <p className={russoOne.className}>Guide the red point through the maze, but don&apos;t touch the walls!</p>
         <button onClick={handleStart}>Play</button>
       </div>
     </div>
