d41a855588ca3e84d300cf61f3a671e47a7435f1
gio <95066222+gioAlgorithm@users.noreply.github.com>
2024-02-15 17:54:19 +0400

    fix profilepage fonts

--- src/app/profilePage/page.tsx
+++ src/app/profilePage/page.tsx
@@ -9,6 +9,12 @@ import Link from 'next/link'
 import { auth} from "../../utils/firebase";
 import HistoryContainer from '@/components/HistoryContainer/HistoryContainer'
 import StatContainer from '@/components/StatContainer/StatContainer'
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin', 'cyrillic', 'cyrillic-ext' ,'greek', 'latin-ext']
+})
 
 // icons
 import { IoMdArrowDropleft } from "react-icons/io";
@@ -36,30 +42,32 @@ const ProfilePage = () => {
 
 
   return (
-    <div className={style.profilePage}>
-      <div className={style.profileContainer}>
-        <div className={style.profileImageContainer}>
-          <div className={style.profileImage}>
-            {user?.photoURL ? <Image id='profileImage' alt='profile' width={100} height={100} src={user?.photoURL} priority/> : <ProfileIcon width={`100`} height={`100`} fontSize={'2.2'} title={user?.displayName}/>}
+    <div className={roboto.className}>
+      <div className={style.profilePage}>
+        <div className={style.profileContainer}>
+          <div className={style.profileImageContainer}>
+            <div className={style.profileImage}>
+              {user?.photoURL ? <Image id='profileImage' alt='profile' width={100} height={100} src={user?.photoURL} priority/> : <ProfileIcon width={`100`} height={`100`} fontSize={'2.2'} title={user?.displayName}/>}
+            </div>
+            <span className={`${style.profileName}  ${loading && style.profileNameLoading}`} id='profileName'>
+              {user?.displayName && user?.displayName}
+            </span>
           </div>
-          <span className={`${style.profileName}  ${loading && style.profileNameLoading}`} id='profileName'>
-            {user?.displayName && user?.displayName}
-          </span>
+          <Link className={style.back} href="/"><IoMdArrowDropleft /> Back</Link>
+          <div className={style.profileContainerFooter}>
+            
+          </div>
         </div>
-        <Link className={style.back} href="/"><IoMdArrowDropleft /> Back</Link>
-        <div className={style.profileContainerFooter}>
-          
-        </div>
-      </div>
 
-      {/* profile page statistic and content */}
-      <div className={style.profilePageContent}>
-        
-        <HistoryContainer gameHistory={gameHistory} loadingSkeleton={loadingSkeleton} />
-        
-        <StatContainer loadingSkeleton={loadingSkeleton} bestTime={bestTime} totalWins={totalWins} totalTime={totalTime} totalGames={totalGames} levelCount={levelCount} leaderboard={leaderboard} />
+        {/* profile page statistic and content */}
+        <div className={style.profilePageContent}>
+          
+          <HistoryContainer gameHistory={gameHistory} loadingSkeleton={loadingSkeleton} />
+          
+          <StatContainer loadingSkeleton={loadingSkeleton} bestTime={bestTime} totalWins={totalWins} totalTime={totalTime} totalGames={totalGames} levelCount={levelCount} leaderboard={leaderboard} />
+        </div>
+        <p className={style.copyright}>Copyright © 2024 Giorgi Machitadze All Rights Reserved.</p>
       </div>
-      <p className={style.copyright}>Copyright © 2024 Giorgi Machitadze All Rights Reserved.</p>
     </div>
   )
 }
